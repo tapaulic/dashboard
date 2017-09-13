34ddb5d2f59fdae3cc5dab9cd1fda33a473202b7
Edgar Min <emin@org.ad.toronto.ca>
2017-09-13 15:37:49 -0400

    Initial version of this branch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -184,7 +184,7 @@ App.prototype.paintDetail = function( indicator ) {
       compVal1 = m.ytds[m.vs[0].y][m.vs[0].p]; compVal2 = 0;
       $.each (this.targets[$(indicator).attr("id")], function(i,item) {if (item.y == m.vs[0].y && item.p <= m.vs[0].p) {compVal2 += item.v;}});
       if (compVal2 != 0) {sHTMLTREND += o.getAnalysis(m, compVal1, compVal2, "Current Year-To-Date vs. Budget/Target", true, true, false, false);}
-    }    
+    }
   }
 
 
@@ -561,10 +561,11 @@ App.prototype.drawSymbol = function(sPOSNEG, sDIRECTION) {
 };
 App.prototype.drawMeasure = function(m, cat) {
   var o = this;
-  var sKEYWORDS, sMEASURE, sVALUE, sINTERVAL, sPOSNEG, sPERIOD, sDIRECTION, sCHANGE, sID, lastVal, lastYear, lastPeriod, intDA;
+  var sKEYWORDS, sMEASURE, sVALUE, sINTERVAL, sPOSNEG, sPERIOD, sDIRECTION, sCHANGE, sID, sIcon,lastVal, lastYear, lastPeriod, intDA;
   var compVal1, compVal2;
   sMEASURE = m.m;
   sKEYWORDS = m.kw;
+  sIcon=m.icon;
 
   intDA = (m.da=="") ? 0 : parseInt(m.da);
 
@@ -641,12 +642,12 @@ App.prototype.drawMeasure = function(m, cat) {
   }
 
   if (m.dd=="None") {sPOSNEG = "N/A";}
-  sCHANGE = (compVal1/compVal2-1) * 100;  
+  sCHANGE = (compVal1/compVal2-1) * 100;
   sCHANGE = Math.abs(sCHANGE.toFixed(2)) + "%";
   sCHANGE = (m.vt=="p") ? ((compVal1 - compVal2) * 100).toFixed(2) + "%" : sCHANGE;
   sID = m.id;
 
-  $( "#cat" + cat.replace(/\W+/g, '')).append( this.createMeasure(sPOSNEG, sKEYWORDS, sMEASURE, sVALUE, sPERIOD, sDIRECTION, sINTERVAL, sCHANGE, sID) );
+  $( "#cat" + cat.replace(/\W+/g, '')).append( this.createMeasure(sPOSNEG, sKEYWORDS, sMEASURE, sVALUE, sPERIOD, sDIRECTION, sINTERVAL, sCHANGE, sID,sIcon) );
 };
 App.prototype.createTab = function (sTabName, index){
   var o = this;
@@ -658,7 +659,7 @@ App.prototype.createTab = function (sTabName, index){
     $( o.selectors.indicators ).append( '<section class="tab-pane" id="cat' + sTabName.replace(/\W+/g, '') + '"></section>' );
   }
 };
-App.prototype.createMeasure = function(strPSN, strKW, strTitle, strVal,strPeriod, strDirection, strInterval, strChangeVal, strID ) {
+App.prototype.createMeasure = function(strPSN, strKW, strTitle, strVal,strPeriod, strDirection, strInterval, strChangeVal, strID,strIcon ) {
   var strHTML = "";
   strHTML += '<div class="aindicator nonactive" href="#" id="' + strID + '"><div class="indicator ' + strPSN + '">';
   strHTML += '<div class="hide keywords">' + strKW + '</div>';
@@ -669,9 +670,13 @@ App.prototype.createMeasure = function(strPSN, strKW, strTitle, strVal,strPeriod
   /* hide measure detail section - replace with icon
 
   <img src="/resources/dashboard/img/line.png" alt="Line chart icon"/>*/
+  if(!strIcon || 0 === strIcon.length){
+    strHTML += '<br><p class="measurevalue"><span>' + strVal + '</span></p>';
+    strHTML += '<p class="measureperiod">' + strPeriod + '</p>';
 
-  strHTML += '<br><p class="measurevalue"><span>' + strVal + '</span></p>';
-  strHTML += '<p class="measureperiod">' + strPeriod + '</p>';
+  }else {
+    strHTML += '<br><p class="measurevalue"><span class="' + strIcon + '"/></p>';
+  }
   strHTML += '<div class="row">';
   strHTML += '<div class="col-xs-12 explanation"><div>';
   //strHTML += (strDirection == "Up2") ? '<p><span class="glyphicon glyphicon-arrow-up"></span></p><p class="direction">Increase of ' + strChangeVal + ' from previous ' + strInterval + '</p>' : '';
